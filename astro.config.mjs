9c0cca83b1eafc648772c1c8f9c958f5cb20e8e7
mokshmodi <mokshmodi96>
2024-01-21 13:24:11 +0530

    ⬆️Upgraded to Astro 4 and removed the Image Component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 export default defineConfig({
   site: 'https://mokshmodi.dev/',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-      {
-        serviceEntryPoint: '@astrojs/image/sharp',
-        cacheDir: "./.cache/image",
-        logLevel: 'debug',
-      }
-  )]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
